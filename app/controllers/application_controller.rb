9a637ea78513049cade4e56957b40a6b4e6283eb
Pawel Ksieniewicz <pawel@ksienie.com>
2019-01-07 03:23:10 +0100

    Authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
+include ActionController::HttpAuthentication::Token::ControllerMethods
+
 class ApplicationController < ActionController::API
+    def authenticate_token
+      authenticate_with_http_token do |token, options|
+        @user ||= User.find_by(token: token)
+      end
+    end
+
+    def render_unauthorized(message)
+      errors = { errors: [ { detail: message } ] }
+      render json: errors, status: :unauthorized
+    end
+
+    def require_token
+      authenticate_token || render_unauthorized("Access denied")
+    end
 end
