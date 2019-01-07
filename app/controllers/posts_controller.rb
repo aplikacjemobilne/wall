8a4b01d3f55bf1131b4bb3be4e088f5e0000d1b6
Pawel Ksieniewicz <pawel@ksienie.com>
2019-01-07 03:28:02 +0100

    Pozostałe akcje.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,15 @@
 class PostsController < ApplicationController
   before_action :require_token
   def post
+    @post = Post.new(post_params)
+    @post.user = @user
+    @post.topic = Topic.from_post(@post)
+
+    if @post.save
+      render status: :created
+    else
+      render :json => @post.errors, status: :bad_request
+    end
   end
 
 private
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,12 @@
 class TopicsController < ApplicationController
   before_action :require_token
   def show
+    unless @topic = Topic.find_by(title: params[:topic_title].downcase)
+      render json: {}, status: :not_found
+    end
   end
 
   def index
+    @topics = Topic.all.sort_by {|topic| topic.posts.length }.reverse
   end
 end
